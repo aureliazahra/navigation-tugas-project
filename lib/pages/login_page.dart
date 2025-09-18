4011a567c97dd1f1c86875db2df4da3d37675343
Aurelia <aurelaurelzr@gmail.com>
2025-09-19 02:29:12 +0700

    login page done

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -8,59 +8,124 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Padding(
-          padding: EdgeInsets.all(30),
+          padding: const EdgeInsets.all(30),
           child: Column(
             children: [
-              Text(
+              const Text(
                 "Sign In",
                 style: TextStyle(
                   fontSize: 28,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              SizedBox(height: 30,),
+              const SizedBox(height: 30),
               TextFormField(
                 decoration: InputDecoration(
                   hintText: 'Username',
-                  border:  OutlineInputBorder(
+                  border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10),
                   ),
                   contentPadding: const EdgeInsets.symmetric(
-                    vertical: 20.0, horizontal: 15.0
-                  ),
+                      vertical: 20.0, horizontal: 15.0),
                 ),
               ),
-              SizedBox(height: 15),
+              const SizedBox(height: 15),
               TextFormField(
                 decoration: InputDecoration(
                   hintText: 'Email',
-                  border:  OutlineInputBorder(
+                  border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10),
                   ),
                   contentPadding: const EdgeInsets.symmetric(
-                    vertical: 20.0, horizontal: 15.0
-                  ),
+                      vertical: 20.0, horizontal: 15.0),
                 ),
               ),
-              SizedBox(height: 15),
+              const SizedBox(height: 15),
               TextFormField(
                 decoration: InputDecoration(
                   hintText: 'Password',
-                  border:  OutlineInputBorder(
+                  border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10),
                   ),
                   contentPadding: const EdgeInsets.symmetric(
-                    vertical: 20.0, horizontal: 15.0
-                  ),
+                      vertical: 20.0, horizontal: 15.0),
                 ),
               ),
-              const SizedBox(height: 15,),
+              const SizedBox(height: 15),
               const Align(
                 alignment: Alignment.centerRight,
                 child: Text(
                   "Forgot Password?",
-                  style: TextStyle(color: Color.fromARGB(255, 60, 0, 199))),
+                  style: TextStyle(color: Color.fromARGB(255, 60, 0, 199)),
                 ),
+              ),
+              const Spacer(),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFF87ADFF),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  padding: const EdgeInsets.symmetric(vertical: 18),
+                  elevation: 4,
+                ),
+                child: const Center(
+                  child: Text(
+                    'Sign In',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15),
+              Row(
+                children: [
+                  Expanded(
+                    child: Divider(
+                      thickness: 1,
+                      color: Colors.grey[100],
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text("OR"),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      thickness: 1,
+                      color: Colors.grey[100],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 15),
+              
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: Image.asset(
+                    "assets/images/google.png", 
+                    height: 24,
+                    width: 24,
+                  ),
+                  label: const Text(
+                    'Login with Google',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    elevation: 3,
+                    padding: const EdgeInsets.symmetric(vertical: 18),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
