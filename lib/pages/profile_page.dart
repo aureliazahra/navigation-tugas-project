ecb3ba5f7de451372e2692db789a1ba11608357c
Aurelia <aurelaurelzr@gmail.com>
2025-09-20 11:17:51 +0700

    add healthcard in profilepage

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -26,9 +26,83 @@ class ProfilePage extends StatelessWidget {
                     radius: 60,
                     backgroundImage: AssetImage('assets/images/profile.jpg'),
                   ),
+                  SizedBox(height: 15),
+                  Text(
+                    'John Doe',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    'johndoe.gmail.com',
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                  ),
                 ],
               ),
-            )
+            ),
+            Padding(
+              padding: const EdgeInsets.all(40),
+              child: Column(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: 140,
+                    padding: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: const Color.fromARGB(26, 112, 0, 0),
+                    ),
+                    child: const Row(
+                      children: [
+                        Icon(Icons.favorite,
+                        color: Color(0xFFD2416E), size: 120,),
+                        SizedBox(width: 40,),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Heart Rate',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w500,
+                                color: Color(0xFFD24166),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 4,
+                            ),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Text(
+                                  '97',
+                                  style: TextStyle(
+                                    fontSize: 50,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color(0xFFD24166),
+                                  ),
+                                ),
+                                SizedBox(width: 4,),
+                                Text(
+                                  'npm',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xFFD24166)
+                                  ),
+                                )
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
